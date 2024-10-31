b7060cd656f54692e0677d25f2c40d20097b7319
Adebambo Tunde <z.anthony@yahoo.com>
2024-10-31 13:55:26 +0100

    feat(schema.prisma): install prisma and initialize

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import * as http from 'http';
 const httpServer: http.Server = http.createServer(app);
 
 //const PORT = config.server.port || 4400;
-const PORT = 5000;
+const PORT = 5500;
 
 httpServer.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
