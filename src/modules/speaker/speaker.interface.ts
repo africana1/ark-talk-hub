9a2b78121c067c92139844ea8c94e57dad2f2dd2
Adebambo Tunde <z.anthony@yahoo.com>
2024-11-04 18:05:26 +0100

    fix(speaker.service.ts): add email & phone validation
    Check if email or phone already exist. throw error if true

--- src/modules/speaker/speaker.interface.ts
+++ src/modules/speaker/speaker.interface.ts
@@ -4,6 +4,8 @@ export interface ISpeakerRepository {
   create(data: NewSpeaker): Promise<Speaker>;
   findAll(): Promise<Speaker[]>;
   findById(id: string): Promise<Speaker | null>;
+  findByEmail(email: string): Promise<Speaker | null>;
+  findByPhone(phone: string): Promise<Speaker | null>;
   update(id: string, data: UpdateSpeaker): Promise<Speaker | null>;
   delete(id: string): Promise<void>;
 }
--- src/modules/speaker/speaker.repository.ts
+++ src/modules/speaker/speaker.repository.ts
@@ -2,35 +2,47 @@ import { PrismaClient } from '@prisma/client';
 import { ISpeakerRepository } from './speaker.interface';
 import { NewSpeaker, Speaker, UpdateSpeaker } from './speaker.types';
 
-const prisma = new PrismaClient();
+const { speaker } = new PrismaClient();
 
 export class SpeakerRepository implements ISpeakerRepository {
   async create(data: NewSpeaker): Promise<Speaker> {
-    return prisma.speaker.create({ data });
+    return speaker.create({ data });
   }
 
   async findAll(): Promise<Speaker[]> {
-    return prisma.speaker.findMany({
+    return speaker.findMany({
       include: { talk: true },
     });
   }
 
   async findById(id: string): Promise<Speaker | null> {
-    return prisma.speaker.findUnique({
+    return speaker.findUnique({
       where: { id },
       include: { talk: true },
     });
   }
 
+  async findByEmail(email: string): Promise<Speaker | null> {
+    return speaker.findUnique({
+      where: { email },
+    });
+  }
+
+  async findByPhone(phone: string): Promise<Speaker | null> {
+    return speaker.findUnique({
+      where: { phone },
+    });
+  }
+
   async update(id: string, data: UpdateSpeaker): Promise<Speaker | null> {
-    return prisma.speaker.update({
+    return speaker.update({
       where: { id },
       data,
     });
   }
 
   async delete(id: string): Promise<void> {
-    await prisma.speaker.delete({
+    await speaker.delete({
       where: { id },
     });
   }
